e73a0cc19be93d12852ae6b429ccf4ebcc12bff6
henriqueos98 <henriqueos98@outlook.com>
2024-11-17 22:16:25 -0300

    adding secrets for firestore and deploying first version

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import json
+from google.oauth2 import service_account
+from google.cloud import firestore
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+# Load the Firestore key from Streamlit secrets
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-todo")
+
+# Streamlit widgets to let a user create a new task
+task = st.text_input("Task")
+submit = st.button("Add Task")
+
+# Once the user has submitted, upload it to the database
+if task and submit:
+    doc_ref = db.collection("tasks").document(task)
+    doc_ref.set({"task": task})
+
+# And then render each task
+tasks_ref = db.collection("tasks")
+for doc in tasks_ref.stream():
+    task = doc.to_dict()["task"]
+
+    st.write(f"- {task}")
